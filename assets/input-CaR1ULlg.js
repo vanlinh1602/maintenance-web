4c28e08b2d27381601024d9afc312750ec17a806
Nguyễn Văn Linh <linhnv1622@gmail.com>
2024-10-29 00:14:13 +0700

    Updates

---
+++ assets/input-CaR1ULlg.js
@@ -0,0 +1,11 @@
+import{c as e,r as a,j as i,v as n}from"./index-CGi-Suzj.js";/**
+ * @license lucide-react v0.452.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=e("Plus",[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]]);/**
+ * @license lucide-react v0.452.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const u=e("Search",[["circle",{cx:"11",cy:"11",r:"8",key:"4ej97u"}],["path",{d:"m21 21-4.3-4.3",key:"1qie3q"}]]),l=a.forwardRef(({className:r,type:s,...t},o)=>i.jsx("input",{type:s,className:n("flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",r),ref:o,...t}));l.displayName="Input";export{l as I,d as P,u as S};
